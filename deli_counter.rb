685abb64805821fed9560083f5f3c6d7f3d2cdee
Isai Bismark Cortez <isai0505@gmail.com>
2019-07-28 15:29:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,5 +16,6 @@ def take_a_number(line, new_person)
   puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end
 
-def now_serving
+def now_serving(next_person)
+  puts "There is nobody waiting to be served!"
 end
