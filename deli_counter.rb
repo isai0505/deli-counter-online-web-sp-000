ff9c5859430809f1e3727848706464daf1ec482f
Isai Bismark Cortez <isai0505@gmail.com>
2019-07-28 15:24:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line(array)
+  if array.length >= 1
+    new_string = 'The line is currently:'
+    array.each.with_index(1) do |name, index|
+    new_string << " #{index}. #{name}"
+    end
+    puts new_string
+  else
+    puts "The line is currently empty."
+  end
+end
